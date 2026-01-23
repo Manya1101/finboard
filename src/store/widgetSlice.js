e0ffdeba65e0e44f34491f2ab88dcb3cedad7440
Manya Aggarwal <manyaaggarwal1103@gmail.com>
2026-01-23 17:50:26 +0530

    Added updateWidget and clearWidgets functionality to widgetSlice

---
+++ src/store/widgetSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const widgetSlice = createSlice({
+  name: "widgets",
+  initialState: {
+    list: [],
+  },
+  reducers: {
+    addWidget: (state, action) => {
+      state.list.push(action.payload);
+    },
+    deleteWidget: (state, action) => {
+      state.list = state.list.filter(
+        (widget) => widget.id !== action.payload
+      );
+    },
+     updateWidget: (state, action) => {
+      const index = state.list.findIndex((w) => w.id === action.payload.id);
+      if (index !== -1)
+        state.list[index] = { ...state.list[index], ...action.payload.data };
+    },
+    clearWidgets: (state) => {
+      state.list = [];
+    },
+  },
+});
+
+export const { addWidget, deleteWidget } = widgetSlice.actions;
+export default widgetSlice.reducer;
